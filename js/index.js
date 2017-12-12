a5b30929eb3017c09206928165a3334fa5b55ec8
Anastasiya Razumeyko <anastasiya.razumeyko@gmail.com>
2017-12-12 10:33:12 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,33 +1,26 @@
 const n = parseInt(prompt('please, enter the Fibonachi number to count'));
 
-document.write('1.by the first way:'+fib(n)+'<br>'+
-               '2.by the second way:'+fib2(n)+'<br>'+
-               '3.by the third way:'+fib3(n));
+document.write('1.by the first way:'+fibRecurs(n)+'<br>'+
+               '2.by the second way:'+fibCircle(n)+'<br>'+
+               '3.by the third way:'+fibArr(n));
 
-function fib(n) {
-    if (n===1) {
-        return 1
-    } else if (n===2) {
-        return 1
-      } else
-         return fib(n-1)+fib(n-2);
+function fibRecurs(n) {
+    if n<=2 ? 1 : fib(n-1)+fib(n-2);
 }
 
-function fib2(n) {
+function fibCircle(n) {
     let prePrev = 1;
     let prev = 1;
     let next = 0;
-    let i=3; 
-        do {
-            next = prePrev + prev;
-            prev = prePrev;
-            next = prev;
-            i++;
-        } while (i<=n);
+    for (let i=3; i<=n; i++;) {
+        next = prePrev + prev;
+        prePrev = prev;
+        prev = next;
+        } 
 return prev;
 }
 
-function fib3(n) {
+function fibArr(n) {
     let nums = [1,1];
     for (let i=2; i<n; i++) {
         nums[i]=nums[i-1]+nums[i-2];
